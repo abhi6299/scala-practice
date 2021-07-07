5800baa131c017b9c57da493e7b6beea12dd1811
Abhinav Goyal <abhinav621999goyal@gmail.com>
2021-07-07 11:40:56 +0530

    Initial commit

---
+++ src/main/scala/com/practice/Example.scala
@@ -0,0 +1,85 @@
+package com.practice
+  import scala.util.control.Breaks.{break, breakable}
+  import util.control.Breaks
+
+object Example {
+  /*def threeSum(nums: Array[Int]): List[List[Int]] = {
+
+    nums.sorted
+    var ans : List[List[Int]] = (List(List(0,0,0)))
+
+    for(i <- 0 to (nums.length-1)) {
+      breakable {
+        if (i > 0 && nums(i) == nums(i - 1)) break
+        var j = i + 1
+        var k = nums.length - 1
+        while (j < k) {
+          val sum : Long = nums(i) + nums (j) + nums (k)
+          if (sum == 0) {
+            val temp: List[Int] = List(nums(i),nums(j),nums(k))
+
+            /*
+            How should I append the list "temp" in ans?
+
+          val b=ans :: temp
+            ans=b.clone()
+
+            */
+
+            while (j < k && nums(j) == nums(j + 1)) {
+              j=j+1
+            }
+
+            while (j < k && nums(k) == nums(k - 1)) {
+              k=k-1
+            }
+
+            j=j+1;
+            k=k-1;
+
+          }
+          else if (sum < 0) {
+            j=j+1
+          }
+          else {
+            k=k-1
+          }
+        }
+      }
+    }
+    ans
+  }*/
+  def reverse(x: Int): Int = {
+
+    var sign = 1
+    if(x<0)
+    {
+      sign = sign * -1
+    }
+
+    var temp = x.toString
+    var i = 0
+    var j = temp.length-1
+    while(i<j)
+    {
+      var a = temp.charAt(i)
+      temp.replace(temp.charAt(i),temp.charAt(j))
+      temp.charAt(j)=a
+      i = i+1
+      j = j-1
+    }
+    var b : Int = temp.toInt
+    b = b * sign
+    b
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val ans : Array[Int] = Array (-1,0,1,2,-1,-4)
+    val t : Int = 6
+    val a = reverse(1234)
+    println(a)
+
+  }
+
+  }
