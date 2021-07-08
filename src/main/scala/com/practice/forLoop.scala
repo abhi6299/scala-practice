ede9a8802f0b7c5eb213d70f06ef79a6c0c9b842
Abhinav Goyal <abhinav621999goyal@gmail.com>
2021-07-08 14:33:39 +0530

    For Loop/yield/if

---
+++ src/main/scala/com/practice/forLoop.scala
@@ -0,0 +1,23 @@
+package com.practice
+
+object forLoop {
+
+  def main(args: Array[String]): Unit ={
+
+    val x = List.range(1,10)
+
+    for(i <- x if i %2 == 0) println(i)
+
+    val y = for{
+      i <- x        // A genrator
+      n = i + 2       // A definintion
+      if (n > 5)      // A filter
+    } yield n
+    println(y)
+
+    val p = for(x <- List(1,2,3); y<- List("10", "2") if x == y.toInt) yield (x,y)
+    println(p)
+
+  }
+
+}
