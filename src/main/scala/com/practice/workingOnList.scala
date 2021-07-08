557f3c160963811ec9ddfbfc90abbf2f49403ee5
Abhinav Goyal <abhinav621999goyal@gmail.com>
2021-07-08 13:26:14 +0530

    Working on lists

--- src/main/scala/com/practice/workingOnList.scala
+++ src/main/scala/com/practice/workingOnList.scala
@@ -1,11 +1,23 @@
 package com.practice
 
+import scala.collection.mutable.ListBuffer
+
 object workingOnList {
 
   def main(args: Array[String]): Unit ={
     var i=0
-
     var temp: List[Int] = List(1)
+
+    var a = new ListBuffer[Int]()
+
+    a += 10
+    a += 20
+
+    val b= a.toList // Converting a BufferList into List
+
+    println(b)
+
+    println(a)
 
     val temp2 = for(x <- temp) yield x*2
 
