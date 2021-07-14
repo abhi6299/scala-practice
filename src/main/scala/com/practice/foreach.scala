a3eb312bcd55108bea8204f67020a2df69db7c28
Abhinav Goyal <abhinav621999goyal@gmail.com>
2021-07-14 12:54:35 +0530

    Foreach/sum

--- src/main/scala/com/practice/foreach.scala
+++ src/main/scala/com/practice/foreach.scala
@@ -8,6 +8,20 @@ object foreach {
     city.foreach(x => println(s"$x $x"))
 
     city.foreach(println)
+    //or
+    city.foreach(o => println(o))
+
+    val x = List(1,2,3,4,5)
+
+    var sum1 = 0
+    x.foreach(i => sum1 += i)
+    println(sum1)
+
+    //or
+
+    var sum2 = 0
+    x.foreach(sum2 += _)
+    println(sum2)
 
   }
 
