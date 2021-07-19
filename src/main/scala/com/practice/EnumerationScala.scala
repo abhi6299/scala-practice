315d6a5e046871a785f66a1216baa620551be1eb
Abhinav Goyal <abhinav621999goyal@gmail.com>
2021-07-19 16:16:04 +0530

    Enumeration In Scala

--- src/main/scala/com/practice/EnumerationScala.scala
+++ src/main/scala/com/practice/EnumerationScala.scala
@@ -2,16 +2,22 @@ package com.practice
 
 object EnumerationScala extends Enumeration{
 
-  type EnumerationScala = Value
+  type Enumeration = Value
   // Assigning values
   val first = Value(0,"Thriller") // Default id = 0
   val second = Value(-1,"Horror") // Default id = 1
   val third = Value(-3,"Comedy") // Default id = 2
   val fourth = Value(10,"Romance") // Default id = 3
 
+  type WeekDays = Value
+  val firs = Value("Mon")
+  val sec = Value("Tues")
+
   def main(args: Array[String]): Unit = {
     println(s"Main Movie Genres = ${EnumerationScala.values}")
     println(s"Main Movie Genres = ${EnumerationScala.third}")
     println(s"Main Movie Genres = ${EnumerationScala.third.id}")
+    println(s"Main Movie Genres = ${EnumerationScala.values}")
+
   }
   }
