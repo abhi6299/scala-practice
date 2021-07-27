a611a083b5c9a3cfa604eb28c056a91002f760ec
Abhinav Goyal <abhinav621999goyal@gmail.com>
2021-07-28 00:29:01 +0530

    Aggregate Functions

---
+++ src/main/scala/com/practice/MinMaxEqualsAbs.scala
@@ -0,0 +1,15 @@
+package com.practice
+
+object MinMaxEqualsAbs {
+
+  def main(args: Array[String]): Unit = {
+
+    println( 4 min 5)
+    println(4 max 5)
+    println(3.abs)
+    println(-3.abs)
+    println( 4 equals 4)
+
+  }
+
+  }
